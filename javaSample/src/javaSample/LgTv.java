2c5a6ca432737bbb6dc16e479568cc8d498d5c8a
blueheeya <blueheeya12@gmail.com>
2024-06-17 11:37:47 +0900

    upload

--- javaSample/src/javaSample/LgTv.java
+++ javaSample/src/javaSample/LgTv.java
@@ -1,5 +1,19 @@
 package javaSample;
 
 public class LgTv {
-
+	public LgTv() {
+		System.out.println("LG TV 생성");
+	}
+	public void turnOn() {
+		System.out.println("LG 켬");
+	}
+	public void turnOff() {
+		System.out.println("LG 끔");
+	}
+	public void soundOn() {
+		System.out.println("LG 소리켬");
+	}
+	public void soundOff() {
+		System.out.println("LG 소리끔");
+	}
 }
---
+++ javaSample/src/javaSample/SamsungTv.java
@@ -0,0 +1,19 @@
+package javaSample;
+
+public class SamsungTv {
+	public SamsungTv() {
+		System.out.println("SM TV 생성");
+	}
+	public void powerOn() {
+		System.out.println("SM 켬");
+	}
+	public void powerOff() {
+		System.out.println("SM 끔");
+	}
+	public void volumeOn() {
+		System.out.println("SM 소리켬");
+	}
+	public void volumeOff() {
+		System.out.println("SM 소리끔");
+	}
+}
